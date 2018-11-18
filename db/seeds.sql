7e24c7493a305a6199b46b8a0141bd7714942531
Alex Young <alexyoungcontact@gmail.com>
2018-11-17 20:05:40 -0600

    working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Kahuna');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeze');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Turkey', true);
